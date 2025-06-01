3934e31725c62074e6b41c92b035c7273e11c4a4
kansha02 <khoneykayla@gmail.com>
2025-06-01 13:00:37 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pages.eda as eda
+import pages.model_page as model_page
+import pages.predict as predict
 
+st.set_page_config(page_title="Dashboard Data Mining", layout="wide")
+st.sidebar.title("Navigasi")
+
+page = st.sidebar.selectbox("Pilih halaman", ("EDA", "Model", "Prediksi"))
+
+if page == "EDA":
+    eda.show()
+elif page == "Model":
+    model_page.show()
+elif page == "Prediksi":
+    predict.show()
